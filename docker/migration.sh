ed24588d5bad6ba2e55223928102ded9c80c6532
paxtonBravo <96046448+paxtonBravo@users.noreply.github.com>
2023-04-19 00:49:09 +0800

    production dockerfile 改進

--- docker/migration.sh
+++ docker/migration.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /var/www/html
-php artisan app:wait_db_alive && php artisan migrate --seed && /usr/bin/supervisord -n -c /etc/supervisord.conf
+php artisan app:wait_db_alive && php artisan migrate --seed --force
